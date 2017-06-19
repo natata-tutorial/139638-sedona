33f54c154aba168bf8e816f315fe0116450fc9e6
Unknown <stmihailov@yandex.ru>
2017-06-20 00:20:01 +0300

    правки

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
-var mqpacker = require("css-mqpacker");
 var server = require("browser-sync").create();
 var rename = require("gulp-rename");
 var imagemin = require("gulp-imagemin");
@@ -13,6 +12,7 @@ var svgstore = require("gulp-svgstore");
 var svgmin = require("gulp-svgmin");
 var del = require("del");
 var minify = require("gulp-csso");
+var mqpacker = require("css-mqpacker");
 
 gulp.task("style", function() {
   gulp.src("sass/style.scss")
