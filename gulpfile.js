0a6dfde85c4402fd38a080f62d82b3f4d4c8248d
Stas <stmihailov@yandex.ru>
2017-06-22 10:18:35 +0300

    правки

--- gulpfile.js
+++ gulpfile.js
@@ -29,7 +29,7 @@ gulp.task("style", function() {
   ]))
   .pipe(gulp.dest("build/css"))
   .pipe(minify())
-  .pipe(rename("style.min.css"))
+  .pipe(rename("style.css"))
   .pipe(gulp.dest("build/css"))
   .pipe(server.stream());
 });
